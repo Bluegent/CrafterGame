77dd8deffd3f789bb63df5c91eea945885524986
Dan Secareanu <dan.secareanu@elektrobit.com>
2019-07-04 11:08:19 +0300

    Initial commit.

---
+++ desktop/src/com/crafter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.crafter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.crafter.CrafterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrafterGame(), config);
+	}
+}
